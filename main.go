432a1b6f894d01e853e2f415800f6081069a8983
Yusuf Ibrahim <mhyusufibrahim@gmail.com>
2017-07-09 20:11:12 +0700

    First commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	pongo "github.com/flosch/pongo2"
+	"github.com/gorilla/mux"
+)
+
+var (
+	templateFolder = "templates/"
+	assetsFolder   = "assets/"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", homeHandler)
+	http.ListenAndServe(":9090", r)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	template := templateFolder + "public/home.html"
+	home_html := pongo.Must(pongo.FromFile(template))
+
+	err := home_html.ExecuteWriter(pongo.Context{"title": "Hello World mate.", "greating": "Hai, Hello world!"}, w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
