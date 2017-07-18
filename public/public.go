6f45f80b8f02b47b810af1d6ccb42c765ef0b986
Yusuf Ibrahim <mhyusufibrahim@gmail.com>
2017-07-18 16:49:27 +0700

    Fix the conflict

--- public/public.go
+++ public/public.go
@@ -14,7 +14,7 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	template := templateFolder + "public/home.html"
 	home_html := pongo2.Must(pongo2.FromFile(template))
 
-	err := home_html.ExecuteWriter(pongo2.Context{"title": "Hello World mate.", "greating": "Hai, Hello world!"}, w)
+	err := home_html.ExecuteWriter(pongo2.Context{"title": "Coming Soon.", "greating": "Hai, Hello world, we're coming soon!"}, w)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
@@ -24,7 +24,7 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	template := templateFolder + "public/login.html"
 	home_html := pongo2.Must(pongo2.FromFile(template))
 
-	err := home_html.ExecuteWriter(pongo2.Context{"title": "Login.", "greating": "Hai, Hello world Login!"}, w)
+	err := home_html.ExecuteWriter(pongo2.Context{"title": "Login.", "greating": "Hai, Hello world Login2!"}, w)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
@@ -34,7 +34,7 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 	template := templateFolder + "public/register.html"
 	home_html := pongo2.Must(pongo2.FromFile(template))
 
-	err := home_html.ExecuteWriter(pongo2.Context{"title": "Register.", "greating": "Hai, Hello world register!"}, w)
+	err := home_html.ExecuteWriter(pongo2.Context{"title": "Register.", "greating": "Hai, Hello world register2!"}, w)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
